ed712184af877b1e54e034ba0bdab171910920ef
HallowAz <amigo_honor@mail.ru>
2022-12-09 17:16:56 +0300

    Loading RK2

--- app/controllers/rk_controller.rb
+++ app/controllers/rk_controller.rb
@@ -2,14 +2,13 @@
 
 class RkController < ApplicationController
   before_action :set_original_arr, only: :view
-  before_action :validate_empty, only: :view 
+  before_action :validate_empty, only: :view
   before_action :validate_last, only: :view
   before_action :validate_alphabet, only: :view
   before_action :validate_array, only: :view
   before_action :validate_all, only: :view
-  
-  def input 
-  end
+
+  def input; end
 
   def set_original_arr
     @original_array = params[:array]
@@ -17,27 +16,26 @@ class RkController < ApplicationController
   end
 
   def validate_alphabet
-    flash[:notice] = 'Не должно быть букв' unless !@original_array.match?(/[A-Za-z]/).nil?
+    flash[:notice] = 'Не должно быть букв' if @original_array.match?(/[A-Za-z]/).nil?
   end
-  
+
   def validate_last
     flash[:notice] = 'Последний символ должен быть числом' unless @original_array.match(/\DZ/).nil?
   end
 
   def validate_empty
-    flash[:notice] = 'Массив не должен быть пустым' unless @original_array.length != 0
+    flash[:notice] = 'Массив не должен быть пустым' if @original_array.empty?
   end
 
   def validate_array
-    flash[:notice] = 'Должны быть массив через пробел' unless !(@original_array.match(/^[\d -]+$/).nil?)
+    flash[:notice] = 'Должны быть массив через пробел' if @original_array.match(/^[\d -]+$/).nil?
   end
 
   def validate_all
     redirect_to home_path unless flash.empty?
   end
-  
-  def view
 
+  def view
     @original_array = @original_array.split(' ').map(&:to_i)
     @transformed_array = transform(@original_array.clone, @k)
   end
@@ -45,7 +43,9 @@ class RkController < ApplicationController
   private
 
   def transform(array, k)
-    array[array.index(array.min)], array[array.index(array.max)], array[k] = array.max, array.min, array.max + array.min
+    array[array.index(array.min)] = array.max
+    array[array.index(array.max)] = array.min
+    array[k] = array.max + array.min
     array
   end
 end
